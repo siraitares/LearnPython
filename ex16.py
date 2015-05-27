4625b98b12fd78d042a7a30328a745606eec8fa9
siraitares <pablo_carranza@hotmail.com>
2015-05-27 01:11:03 -0700

    Add changes up to ex17.py

--- ex16.py
+++ ex16.py
@@ -22,12 +22,7 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
 
-# target.write(line1)
 target.write("%r\n%r\n%r\n" % (line1, line2, line3))
-# target.write(line2)
-# target.write("\n")
-# target.write(line3)
-# target.write("\n")
 
 print "And finally, we close it."
 target.close()
---
+++ ex17.py
@@ -0,0 +1,8 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+open(to_file, 'w').write(open(from_file).read())
+
+print "Alright, all done."
